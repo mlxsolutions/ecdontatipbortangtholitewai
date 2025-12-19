c4aec841fe8b5ef7eed94d0a4edf6a6dd2b279c9
Jax <95479014+mlx-solutions@users.noreply.github.com>
2025-12-19 17:03:01 +0100

    Update ome-reload.sh to use OME_HOST_IP for certificate paths

--- ome/origin/ome-reload.sh
+++ ome/origin/ome-reload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Auto-deploy hook for $DOMAIN
-cp "/etc/letsencrypt/live/$DOMAIN/cert.pem" "$OME_DOCKER_HOME/conf/cert.crt"
-cp "/etc/letsencrypt/live/$DOMAIN/privkey.pem" "$OME_DOCKER_HOME/conf/cert.key"
-cp "/etc/letsencrypt/live/$DOMAIN/chain.pem" "$OME_DOCKER_HOME/conf/cert.ca-bundle"
-cp "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" "$OME_DOCKER_HOME/conf/fullchain.pem"
+cp "/etc/letsencrypt/live/$OME_HOST_IP/cert.pem" "$OME_DOCKER_HOME/conf/cert.crt"
+cp "/etc/letsencrypt/live/$OME_HOST_IP/privkey.pem" "$OME_DOCKER_HOME/conf/cert.key"
+cp "/etc/letsencrypt/live/$OME_HOST_IP/chain.pem" "$OME_DOCKER_HOME/conf/cert.ca-bundle"
+cp "/etc/letsencrypt/live/$OME_HOST_IP/fullchain.pem" "$OME_DOCKER_HOME/conf/fullchain.pem"
 chmod 640 "$OME_DOCKER_HOME/conf/cert.key"
 docker restart ome
