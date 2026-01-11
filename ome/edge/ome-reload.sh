0621b12a1443b16dac370ad27a0daa98b3f158f8
Jax <95479014+mlx-solutions@users.noreply.github.com>
2026-01-11 16:45:56 +0100

    Add Logger.xml, ome-reload.sh, ome-startd.sh, and ome.service for OvenMediaEngine configuration and logging

---
+++ ome/edge/ome-reload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Auto-deploy
+cp "/etc/letsencrypt/live/$OME_HOST_IP/cert.pem" "$OME_DOCKER_HOME/conf/cert.crt"
+cp "/etc/letsencrypt/live/$OME_HOST_IP/privkey.pem" "$OME_DOCKER_HOME/conf/cert.key"
+cp "/etc/letsencrypt/live/$OME_HOST_IP/chain.pem" "$OME_DOCKER_HOME/conf/cert.ca-bundle"
+cp "/etc/letsencrypt/live/$OME_HOST_IP/fullchain.pem" "$OME_DOCKER_HOME/conf/fullchain.pem"
+chmod 640 "$OME_DOCKER_HOME/conf/cert.key"
+docker restart ome
---
+++ ome/edge/ome-startd.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# --- Configuration ---
+OME_DOCKER_HOME="/opt/ovenmediaengine"
+
+# --- Run OME Docker container ---
+echo "Checking environment variables...:"
+if [ -z "$OME_HOST_IP" ]; then
+  echo "❌ Missing required environment variables. Please set OME_HOST_IP."
+  exit 1
+fi
+
+# make sure that is not running
+echo "🧹Stopping and removing any existing OME container..."
+docker stop ome
+docker rm ome
+
+
+# --- Run OME Docker container ---
+echo "🚀 Launching OvenMediaEngine..."
+
+# RUN
+# docker run -d --name ome --restart unless-stopped \
+docker run -d --name ome \
+  -e OME_HOST_IP="$OME_HOST_IP" \
+  -e PUBLIC_IPV4="$PUBLIC_IPV4" \
+  -e API_ACCESS_TOKEN="$API_ACCESS_TOKEN" \
+  -v "$OME_DOCKER_HOME/conf":/opt/ovenmediaengine/bin/origin_conf \
+  -v "$OME_DOCKER_HOME/logs":/var/log/ovenmediaengine \
+  -v "$OME_DOCKER_HOME/rec":/mnt/record \
+  -p 1935:1935 \
+  -p 9999:9999/udp \
+  -p 9000:9000 \
+  -p 3334:3334 \
+  -p 4334:4334 \
+  -p 3478:3478 \
+  -p 8082:8082 \
+  -p 6379:6379 \
+  -p 20081:20081 \
+  -p 10000-10002:10000-10002/udp \
+  airensoft/ovenmediaengine:latest
+
+echo "✅ OME  is up and running!"
