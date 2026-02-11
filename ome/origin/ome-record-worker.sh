f0addab7685fbf6c193e965250056da0e98bda89
Jax <95479014+mlx-solutions@users.noreply.github.com>
2026-02-11 15:44:47 +0100

    Update bitrate calculation in ome-record-worker.sh to use awk for improved precision

--- ome/origin/ome-record-worker.sh
+++ ome/origin/ome-record-worker.sh
@@ -205,7 +205,7 @@ EOF
         DURATION=$(ffprobe -v error -show_entries format=duration \
             -of default=noprint_wrappers=1:nokey=1 "$MOVED_MP4")
         FILESIZE=$(stat -c%s "$MOVED_MP4")
-        BITRATE=$(( FILESIZE * 8 / DURATION ))
+        BITRATE=$(awk "BEGIN { printf \"%.0f\", ($FILESIZE * 8) / $DURATION }")
     fi
 
     RAW_FPS=$(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate \
